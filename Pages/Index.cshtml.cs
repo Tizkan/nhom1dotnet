70394968009eff82302db9ee668ac5ca29a88111
CaHuy77 <trancahuy2005@gmail.com>
2026-06-12 17:32:23 +0700

    up index lan 2

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using nhom1dotnet.Data;
 using nhom1dotnet.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace nhom1dotnet.Pages
 {
@@ -21,6 +22,7 @@ namespace nhom1dotnet.Pages
         
         //Hoạt động gần đây
         public List<ActivityItem> RecentActivities { get; set; } = new();
+
         //Danh sách phòng trống
         public List<Room> AvailableRooms { get; set; } = new();
 
@@ -61,6 +63,7 @@ namespace nhom1dotnet.Pages
             // Danh sách phòng trống
             AvailableRooms = _context.Rooms
                 .Where(r => r.status == "Available")
+                .Include(r => r.RoomType)
                 .Take(10)
                 .ToList();
 
@@ -72,6 +75,12 @@ namespace nhom1dotnet.Pages
         public string Time { get; set; } = "";
         public string Title { get; set; } = "";
         public string CustomerName { get; set; } = "";
+    }
+
+    public class RoomWithTypeName
+    {
+        public int room_number { get; set; }
+        public string type_name { get; set; } = "";
     }
     }
  }
