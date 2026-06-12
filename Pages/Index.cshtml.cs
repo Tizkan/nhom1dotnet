9fc72161c136ea6fa8ede0fe7f9c8c09f84bb62f
CaHuy77 <trancahuy2005@gmail.com>
2026-06-12 14:23:58 +0700

    tao thich the

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,10 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using nhom1dotnet.Data;
+using nhom1dotnet.Models;
 
 namespace nhom1dotnet.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public int TotalBookings { get; set; }
+
         public IActionResult OnGet()
         {
             var user = HttpContext.Session.GetString("username");
@@ -14,7 +25,14 @@ namespace nhom1dotnet.Pages
                 return RedirectToPage("/Login/Login");
             }
 
+            TotalBookings = _context.Bookings.Count();
+
             return Page();
         }
+    }
+
+    internal class ApplicationDbContext
+    {
+        public object Bookings { get; internal set; }
     }
 }
